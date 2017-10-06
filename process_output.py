bf2c7fe0ba933b9b0f7f08e7951679735ffbfec5
sheng wang <wangshengpkucn@gmail.com>
2017-10-06 14:22:42 -0500

    Update process_output.py

--- process_output.py
+++ process_output.py
@@ -1,6 +1,15 @@
-fherb = open('..\..\\output\\topic_model\herbPLSA\\result\\herb.topic13')
-fsym = open('..\..\\output\\topic_model\herbPLSA\\result\\symptom.topic13')
-fout = open('..\..\\output\\herb_symptom\herbPLSA\\herb_symptom','w')
+import sys
+if len(sys.argv<4):
+    print 'wrong argument.'
+    print 'please input: process_output.py herb_file symptom_file output_file'
+    
+#fherb = open('..\..\\output\\topic_model\herbPLSA\\result\\herb.topic13')
+#fsym = open('..\..\\output\\topic_model\herbPLSA\\result\\symptom.topic13')
+#fout = open('..\..\\output\\herb_symptom\herbPLSA\\herb_symptom','w')
+    
+fherb = open(sys.argv[1])
+fsym = open(sys.argv[2])
+fout = open(sys.argv[3],'w')
 d = 20
 for i in range(d):
     fherb.readline()
